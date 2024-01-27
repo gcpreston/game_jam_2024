1b7ce5316c643d8c9e20c110d4de6b76f5f8f0de
Graham Preston <graham.preston@gmail.com>
2024-01-27 13:44:01 -0500

    Sync before break

--- Fishing/Hook.gd
+++ Fishing/Hook.gd
@@ -2,16 +2,30 @@ extends Area2D
 
 signal hit
 
+@export var speed = 400
+var screen_size
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	screen_size = get_viewport_rect().size
 	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
 
 func start(pos):
 	position = pos
